c26631659937bf2fc22ec6cfcf0dd56a6e9bd511
fdzrn <fadzriangibran@gmail.com>
2019-08-01 10:21:37 +0700

    Tugas 24 javascript
    untuk niomic

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+angka = [32,33,16,40,9,7,11,2,44,1,66];
+angkaSorting = angka.sort();
+console.log("Ascending : " + angkaSorting);
+angkaReverse = angka.reverse();
+console.log("Descending : " + angkaReverse);
